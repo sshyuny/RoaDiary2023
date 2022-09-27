b29789fddcf030b640ad3bdcee992f61ad08af63
sshyuny <thgustls@gmail.com>
2022-09-27 17:49:45 +0900

    새 테이블 behavior_priorityofcategory 추가

--- db-init-script/mariadb.sql
+++ db-init-script/mariadb.sql
@@ -40,6 +40,26 @@ CREATE TABLE behavior_records (
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
+CREATE TABLE behavior_priorityofcategory (
+    user_id BIGINT, 
+    n1 BIGINT, n2 BIGINT, n3 BIGINT, n4 BIGINT, n5 BIGINT,
+    n6 BIGINT, n7 BIGINT, n8 BIGINT, n9 BIGINT, n10 BIGINT,
+    n11 BIGINT, n12 BIGINT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (n1) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n2) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n3) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n4) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n5) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n6) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n7) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n8) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n9) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n10) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n11) REFERENCES behavior_category(behavior_category_id), 
+    FOREIGN KEY (n12) REFERENCES behavior_category(behavior_category_id)
+);
+
 INSERT INTO behavior_category
 (content)
 VALUES
@@ -59,3 +79,5 @@ INSERT INTO user
 (email, nickname, recently_visited, register_status)
 VALUES
 ('devsshyuny@gmail.com', 'sshyuny', now(), 'normal');
+
+insert into behavior_priorityofcategory (user_id, n1, n2, n3) values ('1', '1', '2', '3');
