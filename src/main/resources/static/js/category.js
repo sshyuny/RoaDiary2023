ef467aa68c5911abd5536b7edf222b0dbe9864e0
sshyuny <thgustls@gmail.com>
2022-09-29 17:47:12 +0900

    [front] 카테고리 위, 아래로 움직이는 기능 추가중

--- src/main/resources/static/js/category.js
+++ src/main/resources/static/js/category.js
@@ -17,24 +17,31 @@ function makeCategoryTable() {
                 let categoryTdDel = document.createElement("td");
                 let categoryTdModify = document.createElement("td");
                 let categoryTdDelButton = document.createElement("button");
-                let categoryTdModifyButton = document.createElement("button");
+                let categoryTdUpButton = document.createElement("button");
+                let categoryTdDownButton = document.createElement("button");
 
                 // 변수
                 let categoryId = data[i].id;
-                let categoryName = data[i].content
+                let categoryName = data[i].content;
                 let idx = i;
 
                 // 요소에 내용 추가
                 categoryTdName.textContent = categoryName;
                 categoryTdName.id ="categoryTdId" + i;
-                categoryTdModifyButton.innerText = "수정";
-                categoryTdModifyButton.onclick = function() { modifyCategory(idx, categoryName, categoryId); };
                 categoryTdDelButton.innerHTML = "삭제" + data[i].id;
                 categoryTdDelButton.onclick = function() { deleteCategory(categoryId); };
+                categoryTdUpButton.innerText = "위로";
+                categoryTdUpButton.onclick = function() { upCategory(categoryId); };
+                categoryTdDownButton.innerText = "아래로";
+                categoryTdDownButton.onclick = function() { downCategory(categoryId); };
+
+                categoryTdDelButton.setAttribute('class', 'btn btn-secondary btn-outline-danger');
+                categoryTdUpButton.setAttribute('class', 'btn btn-secondary');
+                categoryTdDownButton.setAttribute('class', 'btn btn-secondary');
 
                 // 붙이기
                 categoryTdDel.appendChild(categoryTdDelButton);
-                categoryTdModify.appendChild(categoryTdModifyButton);
+                categoryTdModify.append(categoryTdUpButton, categoryTdDownButton);
                 categoryTr.append(categoryTdName, categoryTdModify, categoryTdDel);
                 categoryTableElem.appendChild(categoryTr);
             }
@@ -45,6 +52,23 @@ function makeCategoryTable() {
     })
 }
 
+
+function deleteCategory(categoryId) {
+    alert(categoryId);
+}
+
+function upCategory(categoryId) {
+    alert(categoryId);
+}
+
+function downCategory(categoryId) {
+    alert(categoryId);
+}
+
+
+
+
+
 // 카테고리 이름 수정
 function modifyCategory(idx, categoryName, categoryId) {
     // 테이블에 있는 카테고리 이름 가져와서, type과 id 변경
@@ -53,7 +77,6 @@ function modifyCategory(idx, categoryName, categoryId) {
                                 + "placeholder='" + categoryName + "' " 
                                 + "onkeydown='reqModifyCategory(event, "+ idx + ", " + categoryId +")' " + "'>";
 }
-
 // 카테고리 이름 수정 - 요청
 function reqModifyCategory(event, idx, categoryId) {
     // 엔터 누를 경우 실행
@@ -62,8 +85,3 @@ function reqModifyCategory(event, idx, categoryId) {
         alert(categoryId + " " + newCategoryName);
     }
 }
-
-function deleteCategory(categoryId) {
-    alert(categoryId);
-}
-
