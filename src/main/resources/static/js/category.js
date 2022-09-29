6710c4b82a946e3cb6f1bd174e46e54b675ba910
sshyuny <thgustls@gmail.com>
2022-09-30 01:00:07 +0900

    [front] category 순위 목록 보여주는 방식 변경

--- src/main/resources/static/js/category.js
+++ src/main/resources/static/js/category.js
@@ -7,51 +7,119 @@ function makeCategoryTable() {
 		method: 'GET',
 		dataType: 'json',
 		success: function(data) {
-            let categoryLength = data.length;
-            let categoryTableElem = document.getElementById("categoryTable");
-
-            for (var i = 0; i < categoryLength; i++) {
-                // 요소 생성
-                let categoryTr = document.createElement("tr");
-                let categoryTdName = document.createElement("td");
-                let categoryTdDel = document.createElement("td");
-                let categoryTdModify = document.createElement("td");
-                let categoryTdDelButton = document.createElement("button");
-                let categoryTdUpButton = document.createElement("button");
-                let categoryTdDownButton = document.createElement("button");
-
-                // 변수
-                let categoryId = data[i].id;
-                let categoryName = data[i].content;
-                let idx = i;
-
-                // 요소에 내용 추가
-                categoryTdName.textContent = categoryName;
-                categoryTdName.id ="categoryTdId" + i;
-                categoryTdDelButton.innerHTML = "삭제" + data[i].id;
-                categoryTdDelButton.onclick = function() { deleteCategory(categoryId); };
-                categoryTdUpButton.innerText = "위로";
-                categoryTdUpButton.onclick = function() { upCategory(categoryId); };
-                categoryTdDownButton.innerText = "아래로";
-                categoryTdDownButton.onclick = function() { downCategory(categoryId); };
-
-                categoryTdDelButton.setAttribute('class', 'btn btn-secondary btn-outline-danger');
-                categoryTdUpButton.setAttribute('class', 'btn btn-secondary');
-                categoryTdDownButton.setAttribute('class', 'btn btn-secondary');
-
-                // 붙이기
-                categoryTdDel.appendChild(categoryTdDelButton);
-                categoryTdModify.append(categoryTdUpButton, categoryTdDownButton);
-                categoryTr.append(categoryTdName, categoryTdModify, categoryTdDel);
-                categoryTableElem.appendChild(categoryTr);
-            }
+            makeCategoryTableSuccess(data);
         }, 
         error: function() {
 			alert("데이터를 가져오는 중 에러가 발생했습니다.");
 		}
     })
 }
+function makeCategoryTableSuccess(data) {
+    let categoryLength = data.length;
+    let categoryListElem = document.getElementById("categoryList");
 
+    for (var i = 0; i < categoryLength; i++) {
+        // 요소 생성
+        let categoryBtnDel = document.createElement("button");
+        let categoryBtnUp = document.createElement("button");
+        let categoryBtnDown = document.createElement("button");
+
+        //div 생성
+        let divRow = document.createElement("div");
+        let divColIdx = document.createElement("div");
+        let divColContent = document.createElement("div");
+        let divColDelBtn = document.createElement("div");
+        let divColUpBtn = document.createElement("div");
+        let divColDownBtn = document.createElement("div");
+        divRow.setAttribute('class', 'row mb-3');
+        divColIdx.setAttribute('class', 'col-1');
+        divColContent.setAttribute('class', 'col');
+        divColDelBtn.setAttribute('class', 'col-2');
+        divColUpBtn.setAttribute('class', 'col-2');
+        divColDownBtn.setAttribute('class', 'col-2');
+
+        // 변수
+        let categoryId = data[i].id;
+        let categoryName = data[i].content;
+        let idx = i;
+
+        // 요소에 내용 추가
+        divColIdx.textContent = idx + 1;
+        divColContent.textContent = categoryName;
+        divColContent.id ="categoryTdId" + i;
+        categoryBtnDel.innerHTML = "삭제" + data[i].id;
+        categoryBtnDel.onclick = function() { deleteCategory(categoryId); };
+        categoryBtnUp.innerText = "위로";
+        categoryBtnUp.onclick = function() { upCategory(categoryId); };
+        categoryBtnDown.innerText = "아래로";
+        categoryBtnDown.onclick = function() { downCategory(categoryId); };
+
+        // 붙이기
+        divColDelBtn.appendChild(categoryBtnDel);
+        divColUpBtn.appendChild(categoryBtnUp);
+        divColDownBtn.appendChild(categoryBtnDown);
+        divRow.append(divColIdx, divColContent, divColUpBtn, divColDownBtn, divColDelBtn);
+        categoryListElem.appendChild(divRow);
+
+        // 디자인: BootStrap 내용
+        categoryBtnDel.setAttribute('class', 'btn btn-secondary btn-outline-danger');
+        categoryBtnUp.setAttribute('class', 'btn btn-secondary');
+        categoryBtnDown.setAttribute('class', 'btn btn-secondary');
+    }
+}
+/*function makeCategoryTableSuccess(data) {
+    let categoryLength = data.length;
+    let categoryTableElem = document.getElementById("categoryTable");
+
+    for (var i = 0; i < categoryLength; i++) {
+        // 요소 생성
+        let categoryTr = document.createElement("tr");
+        let categoryTdName = document.createElement("td");
+        let categoryTdDel = document.createElement("td");
+        let categoryTdUp = document.createElement("td");
+        let categoryTdDown = document.createElement("td");
+        let categoryBtnDel = document.createElement("button");
+        let categoryBtnUp = document.createElement("button");
+        let categoryBtnDown = document.createElement("button");
+
+        //div 생성
+        let divRow = document.createElement("div");
+        let divCol1 = document.createElement("div");
+        let divColDelBtn = document.createElement("div");
+        let divColUpBtn = document.createElement("div");
+        divRow.setAttribute('class', 'row');
+        divCol1.setAttribute('class', 'col');
+        divColDelBtn.setAttribute('class', 'col');
+        divColUpBtn.setAttribute('class', 'col');
+
+        // 변수
+        let categoryId = data[i].id;
+        let categoryName = data[i].content;
+        let idx = i;
+
+        // 요소에 내용 추가
+        categoryTdName.textContent = categoryName;
+        categoryTdName.id ="categoryTdId" + i;
+        categoryBtnDel.innerHTML = "삭제" + data[i].id;
+        categoryBtnDel.onclick = function() { deleteCategory(categoryId); };
+        categoryBtnUp.innerText = "위로";
+        categoryBtnUp.onclick = function() { upCategory(categoryId); };
+        categoryBtnDown.innerText = "아래로";
+        categoryBtnDown.onclick = function() { downCategory(categoryId); };
+
+        // 붙이기
+        categoryTdDel.appendChild(categoryBtnDel);
+        categoryTdUp.append(categoryBtnUp);
+        categoryTdDown.append(categoryBtnDown);
+        categoryTr.append(categoryTdName, categoryTdUp, categoryTdDown, categoryTdDel);
+        categoryTableElem.appendChild(categoryTr);
+
+        // 디자인: BootStrap 내용
+        categoryBtnDel.setAttribute('class', 'btn btn-secondary btn-outline-danger');
+        categoryBtnUp.setAttribute('class', 'btn btn-secondary');
+        categoryBtnDown.setAttribute('class', 'btn btn-secondary');
+    }
+}*/
 
 function deleteCategory(categoryId) {
     alert(categoryId);
